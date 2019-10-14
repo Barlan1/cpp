dd899ee74bcf2e40f632640f73b5a8ec3325671e
Vikas Meena <vikasbarlan1@gmail.com>
2019-10-14 12:39:39 +0530

    Create vector_int_string.cpp

---
+++ vector_int_string.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+int main()
+{
+vector<int> v;
+vector<string> s;
+v.push_back(1);
+v.push_back(12);
+v.push_back(4);
+s.push_back("hello vikas");
+for(vector<int>::iterator a=v.begin(); 
