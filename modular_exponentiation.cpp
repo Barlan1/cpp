f31475149794da51ebd948cafffe794cbe988651
Vikas Meena <vikasbarlan1@gmail.com>
2019-12-21 00:15:46 +0530

    Create modular_exponentiation.cpp

---
+++ modular_exponentiation.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int power( int x, unsigned int y)
+{
+  int res = 1;
+ while(y >0)
+  {
+   if( y & 1)
+     res = rex*x;
+   
+   y = y >> 1;
+   x = x * x;
+  }
+  return res;
+}
+int main()
+{
+int x = 3;
+ int y = 5;
+ power( x,y);
+ return 0;
+ }
+ 
+ 
